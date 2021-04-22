1f81213d6b2ec06673a591ee49ffb7192bafbb10
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2021-04-22 17:14:45 +0300

    Добавьте файлы проекта.

---
+++ xamarin_drag_n_drop/xamarin_drag_n_drop/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_drag_n_drop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+        {
+            var label = (sender as Element)?.Parent as Label;
+            e.Data.Properties.Add("Text", label.Text);
+        }
+
+        private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+        {
+            var data = e.Data.Properties["Text"].ToString();
+            //e.Data.GetTextAsync();
+            var frame = (sender as Element)?.Parent as Frame;
+            frame.Content = new Label
+            {
+                Text = data
+            };
+        }
+    }
+}
